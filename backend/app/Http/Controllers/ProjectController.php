40457784f25d6da08c8f96e5f187f32c40331a82
YusukeSato <yskprog022151@gmail.com>
2020-08-26 14:39:15 +0900

    indexメソッド作成

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Model\Project;
 
 class ProjectController extends Controller
 {
-    //
+    public function index()
+    {
+        $projects = Project::all();
+
+        return response()->json($projects, 200);
+    }
 }
