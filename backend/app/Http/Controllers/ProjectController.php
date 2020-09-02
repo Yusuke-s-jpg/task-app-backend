3b775e78bc8998578f261b55462343e586b9e1cf
YusukeSato <yskprog022151@gmail.com>
2020-09-02 17:58:47 +0900

    storeメソッド追加

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Model\Project;
+use App\Http\Requests\Projects\ProjectStoreRequest;
 
 class ProjectController extends Controller
 {
@@ -12,5 +13,32 @@ class ProjectController extends Controller
         $projects = Project::all();
 
         return response()->json($projects, 200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  ProjectStoreRequest       $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(ProjectStoreRequest $request)
+    {
+        $project = new Project();
+
+        \DB::beginTransaction();
+
+        try {
+            $project->fill($request->all());
+            $project->save();
+
+            \DB::commit();
+        } catch (\Exception $e) {
+            \Log::Error($e->getTraceAsString());
+            \DB::rollback();
+
+            return response()->json('create error', 500);
+        }
+
+        return response()->json($project, 201);
     }
 }
