504ec07c6f00e5428da9101ed059ffa41d301b23
YusukeSato <yskprog022151@gmail.com>
2020-09-20 23:15:35 +0900

    show,update,destroyメソッド作成

--- backend/app/Http/Controllers/ProjectController.php
+++ backend/app/Http/Controllers/ProjectController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Model\Project;
 use App\Http\Requests\Projects\ProjectStoreRequest;
+use App\Http\Requests\Projects\ProjectUpdateRequest;
 
 class ProjectController extends Controller
 {
@@ -15,8 +16,18 @@ class ProjectController extends Controller
         return response()->json($projects, 200);
     }
 
+    public function show($id)
+    {
+        $project = Project::find($id);
+
+        if (is_null($project)) {
+            return response()->json('project not found', 404);
+        }
+
+        return response()->json($project, 200);
+    }
+
     /**
-     * Store a newly created resource in storage.
      *
      * @param  ProjectStoreRequest       $request
      * @return \Illuminate\Http\Response
@@ -40,5 +51,59 @@ class ProjectController extends Controller
         }
 
         return response()->json($project, 201);
+    }
+
+    /**
+     *
+     * @param  ProjectUpdateRequest       $request
+     * @return \Illuminate\Http\Response
+     */
+    public function update(ProjectUpdateRequest $request, $id)
+    {
+        $project = Project::find($id);
+
+        if (is_null($project)) {
+            return response()->json('project is not found', 404);
+        }
+
+        \DB::beginTransaction();
+
+        try {
+            $project->fill($request->all());
+            $project->save();
+
+            \DB::commit();
+        } catch (\Exception $e) {
+            \Log::Error($e->getTraceAsString());
+            \DB::rollback();
+
+            return response()->json('create error', 500);
+        }
+
+        return response()->json($project, 200);
+    }
+
+    public function destroy($id)
+    {
+        $project = Project::find($id);
+
+        if (is_null($project)) {
+            return response()->json('project not found', 404);
+        }
+
+        \DB::beginTransaction();
+
+        try {
+            $project->delete();
+
+            \DB::commit();
+        } catch (\Exception $e) {
+            \Log::Error($e->getTraceAsString());
+            \DB::rollback();
+
+            return response()->json('destroy error', 500);
+        }
+
+        return response()->json(200);
     }
 }
--- backend/tests/Feature/ProjectTest.php
+++ backend/tests/Feature/ProjectTest.php
@@ -20,37 +20,91 @@ class ProjectTest extends TestCase
     // index
     ///////////////////////
 
+    /** 
+     * status:200
+    */
     /** @test */
     public function getAllProjects()
     {
-        $this->projects = factory(Project::class, 10)->create();
+        $projects = factory(Project::class, 10)->create();
         $response = $this->json('GET', 'api/projects');
 
         $response->assertStatus(200)
                  ->assertJsonFragment(
                      [
-                         'id' => $this->projects[0]->id,
-                         'title' => $this->projects[0]->title,
-                         'description' => $this->projects[0]->description,
-                         'state' => $this->projects[0]->state,
-                         'user_id' => $this->projects[0]->user_id,
+                         'id' => $projects[0]->id,
+                         'title' => $projects[0]->title,
+                         'description' => $projects[0]->description,
+                         'state' => $projects[0]->state,
+                         'user_id' => $projects[0]->user_id,
                      ],
                      [
-                        'id' => $this->projects[9]->id,
-                        'title' => $this->projects[9]->title,
-                        'description' => $this->projects[9]->description,
-                        'state' => $this->projects[9]->state,
-                        'user_id' => $this->projects[9]->user_id,
+                        'id' => $projects[9]->id,
+                        'title' => $projects[9]->title,
+                        'description' => $projects[9]->description,
+                        'state' => $projects[9]->state,
+                        'user_id' => $projects[9]->user_id,
                     ]
                  );
     }
+
+    ///////////////////////
+    // show
+    ///////////////////////
+
+    /** 
+     * status:200
+    */
+    /** @test */
+    public function showProject()
+    {
+        $project = factory(Project::class)->create();
+        $project_id = $project->id;
+        $response = $this->json('GET', "api/projects/{$project_id}");
+
+        $response->assertStatus(200)
+                 ->assertJsonFragment(
+                     [
+                         'id' => $project->id,
+                         'title' => $project->title,
+                         'description' => $project->description,
+                         'state' => $project->state,
+                         'user_id' => $project->user_id
+                     ]
+                 );
+    }
     
+    /** 
+     * status:404
+    */
+    /** @test */
+    public function showProjectWhenTheIdDoseNotExist()
+    {
+        $project = factory(Project::class)->create();
+        $project_id = $project->id + 1;
+        $response = $this->json('GET', "api/projects/{$project_id}");
+
+        $response->assertStatus(404)
+                 ->assertJsonMissing(
+                     [
+                         'id' => $project->id,
+                         'title' => $project->title,
+                         'description' => $project->description,
+                         'state' => $project->state,
+                         'user_id' => $project->user_id,
+                     ]
+                 );
+    }
+
     ///////////////////////
     //  store
     ///////////////////////
 
+    /**
+     * status:201
+     */
     /** @test */
-    public function createProject()
+    public function storeProject()
     {
         $projectCount = Project::count();
         $params = [
@@ -69,7 +123,158 @@ class ProjectTest extends TestCase
                     'user_id' => 1
                  ]);
 
-        // Check if the record is added
+        // レコードが追加されているかチェック
         $this->assertSame(Project::count(), $projectCount + 1);
+    }
+
+    ///////////////////////
+    //  update
+    ///////////////////////
+
+    /**
+     * status:200
+     */
+    /** @test */
+    public function updateProject()
+    {
+        $projectCount = Project::count();
+
+        // オリジナルデータ作成
+        $params = [
+            'title' => 'testTitle',
+            'description' => 'testDescription',
+            'state' => 'progress',
+            'user_id' => 1
+        ];
+        $response = $this->json('POST', 'api/projects', $params);
+
+        $project = Project::where('title', 'testTitle')->first();
+
+        // アップデート用データ作成
+        $params = [
+            'title' => 'updatedTitle',
+            'description' => 'updatedDescription',
+            'state' => 'progress',
+            'user_id' => 1
+        ];
+        $response = $this->json('PUT', "api/projects/{$project->id}", $params);
+
+        $response->assertStatus(200);
+        $this->assertDatabaseHas('projects', [
+                'title' => 'updatedTitle',
+                'description' => 'updatedDescription',
+                'state' => 'progress',
+                'user_id' => 1
+             ])
+             ->assertDatabaseMissing('projects', [
+                'title' => 'testTitle',
+                'description' => 'testDescription',
+                'state' => 'progress',
+                'user_id' => 1
+             ]);
+
+        // レコードが追加されていないことを確認
+        $this->assertSame(Project::count(), $projectCount + 1);
+    }
+
+    /**
+     * status:404
+     */
+    /** @test */
+    public function updateProjectWhenTheIdDoseNotExist()
+    {
+        $projectCount = Project::count();
+
+        // オリジナルデータ作成
+        $params = [
+            'title' => 'testTitle',
+            'description' => 'testDescription',
+            'state' => 'progress',
+            'user_id' => 1
+        ];
+        $response = $this->json('POST', 'api/projects', $params);
+
+        $project = Project::where('title', 'testTitle')->first();
+        $project_id = $project->id + 1;
+
+        // アップデート用データ作成
+        $params = [
+            'title' => 'updatedTitle',
+            'description' => 'updatedDescription',
+            'state' => 'progress',
+            'user_id' => 1
+        ];
+        $response = $this->json('PUT', "api/projects/{$project_id}", $params);
+
+        $response->assertStatus(404);
+        $this->assertDatabaseHas('projects', [
+                'title' => 'testTitle',
+                'description' => 'testDescription',
+                'state' => 'progress',
+                'user_id' => 1
+             ])
+             ->assertDatabaseMissing('projects', [
+                'title' => 'updatedTitle',
+                'description' => 'updatedDescription',
+                'state' => 'progress',
+                'user_id' => 1
+             ]);
+
+        // レコードが追加されていないことを確認
+        $this->assertSame(Project::count(), $projectCount + 1);
+    }
+
+    ///////////////////////
+    // delete
+    ///////////////////////
+
+    /** 
+     * status:200
+    */
+    /** @test */
+    public function deleteProject()
+    {
+        $project = factory(Project::class)->create();
+        $project_id = $project->id;
+        $projectCount = Project::count();
+        $response = $this->json('DELETE', "api/projects/{$project_id}");
+
+        $response->assertStatus(200);
+        $this->assertDatabaseMissing('projects', 
+                    [
+                        'id' => $project->id,
+                        'title' => $project->title,
+                        'description' => $project->description,
+                        'state' => $project->state,
+                        'user_id' => $project->user_id
+                    ]);
+
+        // レコードが削除されていることを確認
+        $this->assertSame(Project::count(), $projectCount - 1);
+    }
+
+    /** 
+     * status:404
+    */
+    /** @test */
+    public function deleteProjectWhenTheIdDoseNotExist()
+    {
+        $project = factory(Project::class)->create();
+        $project_id = $project->id + 1;
+        $projectCount = Project::count();
+        $response = $this->json('DELETE', "api/projects/{$project_id}");
+
+        $response->assertStatus(404);
+        $this->assertDatabaseHas('projects', 
+                    [
+                        'id' => $project->id,
+                        'title' => $project->title,
+                        'description' => $project->description,
+                        'state' => $project->state,
+                        'user_id' => $project->user_id
+                    ]);
+
+        // レコードが削除されていないことを確認
+        $this->assertSame(Project::count(), $projectCount);
     }
 }
