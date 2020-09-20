0e37e95f8463b7cf0a565f0c9124101be603933f
YusukeSato <yskprog022151@gmail.com>
2020-09-20 23:42:39 +0900

    seeder作成

--- backend/database/seeds/ProjectsTableSeeder.php
+++ backend/database/seeds/ProjectsTableSeeder.php
@@ -11,6 +11,50 @@ class ProjectsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('projects')->insert([
+            'id'          => 1,
+            'title'       => 'プロジェクト1',
+            'description' => '内容',
+            'state'       => 'progress',
+            'user_id'     => 1,
+            'created_at'  => now(),
+            'updated_at'  => now()
+        ]);
+        DB::table('projects')->insert([
+            'id'          => 2,
+            'title'       => 'プロジェクト2',
+            'description' => '内容',
+            'state'       => 'progress',
+            'user_id'     => 1,
+            'created_at'  => now(),
+            'updated_at'  => now()
+        ]);
+        DB::table('projects')->insert([
+            'id'          => 3,
+            'title'       => 'プロジェクト3',
+            'description' => '内容',
+            'state'       => 'progress',
+            'user_id'     => 1,
+            'created_at'  => now(),
+            'updated_at'  => now()
+        ]);
+        DB::table('projects')->insert([
+            'id'          => 4,
+            'title'       => 'プロジェクト4',
+            'description' => '内容',
+            'state'       => 'progress',
+            'user_id'     => 1,
+            'created_at'  => now(),
+            'updated_at'  => now()
+        ]);
+        DB::table('projects')->insert([
+            'id'          => 5,
+            'title'       => 'プロジェクト5',
+            'description' => '内容',
+            'state'       => 'progress',
+            'user_id'     => 1,
+            'created_at'  => now(),
+            'updated_at'  => now()
+        ]);
     }
 }
